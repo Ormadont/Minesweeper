0f7feea6e3724452b25f4d43aaea593eb2538128
Ormadont <evgeniy1demyanov@gmail.com>
2018-04-25 15:26:28 +0400

    refactoring - all functions insert into two classes: board and game

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,10 +1,22 @@
 class Game {
-  constructor(numberOfRows, numberOfColumns, _numberOfBombs) {
+  constructor(numberOfRows, numberOfColumns, numberOfBombs) {
     this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
-  }
-  
-}
+  };
 
+  playMove(rowIndex, columnIndex) {
+    this._board.flipTile(rowIndex, columnIndex);
+    if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {  //If the flipped tile has a bomb, the game is over
+      console.log('game is over');
+      this._board.printBoard();;
+    } else if (this._board.hasSafeTiles()) { //board does not have any safe tiles left, the player has won the game
+      console.log('congratulates the user on winning');
+    } else { //player should be allowed to continue playing
+      console.log('Current Board');
+      this._board.printBoard();
+    }
+  };
+
+}
 
 class Board {
   //start class
@@ -12,8 +24,8 @@ class Board {
   constructor(numberOfRows, numberOfColumns, numberOfBombs) {
     this._numberOfBombs = numberOfBombs;
     this._numberOfTiles = numberOfRows * numberOfColumns;
-    this._playerBoard   = generatePlayerBoard(numberOfRows, numberOfColumns);
-    this._bombBoard     = generateBombBoard(numberOfRows, numberOfColumns, numberOfBombs);
+    this._playerBoard   = Board.generatePlayerBoard(numberOfRows, numberOfColumns);
+    this._bombBoard     = Board.generateBombBoard(numberOfRows, numberOfColumns, numberOfBombs);
   };
 
   get playerBoard() {
@@ -28,7 +40,8 @@ class Board {
       this._playerBoard[rowIndex][columnIndex] = 'B';
     }
     else {
-      this._playerBoard[rowIndex][columnIndex] = getNumberOfNeighborBombs(this._bombBoard, rowIndex, columnIndex);
+      this._playerBoard[rowIndex][columnIndex] =
+      this.getNumberOfNeighborBombs( rowIndex, columnIndex);
     }
     this._numberOfTiles--;
   };
@@ -37,18 +50,18 @@ class Board {
     const neighborOffsets =  [[1,1],[1,-1],[-1,1],[-1,-1],[0,1],[0,-1],[1,0],[-1,0]];
     const numberOfRows = this._bombBoard.length;
     const numberOfColumns = this._bombBoard[0].length;
-    numberOfBombs = 0;
+    this._numberOfBombs = 0;
 
     neighborOffsets.forEach(offset => {
       const rowIndex1 = rowIndex+offset[0];
       const columnIndex1 = columnIndex+offset[1];
       if (rowIndex1>=0 && columnIndex1>=0 && rowIndex1<numberOfRows && columnIndex1<numberOfColumns) {
         if (this._bombBoard[rowIndex1][columnIndex1] === 'B') {
-          numberOfBombs++;
+          this._numberOfBombs++;
         }
       }
     })
-    return numberOfBombs;
+    return this._numberOfBombs;
   };
 
   hasSafeTiles() { //informing the user that they've won the game
@@ -92,7 +105,10 @@ class Board {
     return board;
   };
 
-
-
   //end of class
 }
+
+const g = new Game(3,3,2);
+g.playMove(0,0);
+g.playMove(1,1);
+g.playMove(2,2);
